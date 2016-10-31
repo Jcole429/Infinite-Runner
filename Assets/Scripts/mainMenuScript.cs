77fb23948a1c11020a47ef0ac1feb7d8eda76562
Jcole429 <Jcole429@student.fdu.edu>
2016-10-31 12:50:47 -0400

    Initial Commit

---
+++ Assets/Scripts/mainMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class mainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void NoviceButtonClick(){
+		SceneManager.LoadScene ("StraightPathsLevel");
+	}
+	public void AdventurerButtonClick(){
+		SceneManager.LoadScene ("RotatedPathsLevel");
+	}
+	public void QuitButtonClick(){
+		Application.Quit ();
+	}
+}
